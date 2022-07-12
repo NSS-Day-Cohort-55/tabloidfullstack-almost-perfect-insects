be75b96d63fb33c0f342a49ea4c69b99cba4c51e
Olivia Lind <eo.lind@gmail.com>
2022-07-12 14:33:41 -0500

    initial commit & view all posts ticket server side code

---
+++ Tabloid/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Tabloid.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
